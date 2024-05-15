6e2b3a10448641d8bdf144390c21ee68b7af7f55
Carlos Abarca Vega <soporte@compuelec.cl>
2024-05-15 10:34:25 -0400

    Add Dockerfile and refactor

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
-import { Module } from '@nestjs/common';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WorkyNotificationsModule } from './worky-notifications/worky-notifications.module';
+import * as cors from 'cors';
 
 @Module({
   imports: [WorkyNotificationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(cors()).forRoutes('*');
+  }
+}
--- src/worky-notifications/worky-notificationsComment.gateway.ts
+++ src/worky-notifications/worky-notificationsComment.gateway.ts
@@ -1,23 +1,20 @@
-import { WebSocketGateway, SubscribeMessage, MessageBody, WebSocketServer} from '@nestjs/websockets';
-import { Server } from "socket.io";
+import {
+  WebSocketGateway,
+  SubscribeMessage,
+  MessageBody,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server } from 'socket.io';
 import { CommentI } from 'src/interfaces/comments/comments.interface';
 
-@WebSocketGateway(81, {cors: {origin: '*'}})
+@WebSocketGateway({ port: Number(process.env.WS_PORT), cors: { origin: '*' } })
 export class WorkyNotificationsGateway {
   constructor() {}
   @WebSocketServer()
-    server: Server
-  
+  server: Server;
+
   @SubscribeMessage('newComment')
-  handleNewComment(
-    @MessageBody() payload: CommentI,){
-    const commentId = payload.commentId;
-    const publicacionId = payload.publicacionId;
-    const userEmittedId = payload.userEmittedId;
-    const authorPublicationId = payload.userReceiveId;
-
-    //this.workyNotificationsService.sendNotificationToUser(userReceiveId, userEmittedId, publicacionId, commentId);
+  handleNewComment(@MessageBody() payload: CommentI) {
     this.server.emit('newComment', payload);
-}
-
+  }
 }
