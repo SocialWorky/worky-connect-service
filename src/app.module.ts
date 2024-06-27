3bf3ba7473116498e421f9b7a47aa0d0e6ecc717
Carlos Abarca Vega <soporte@compuelec.cl>
2024-06-27 11:46:11 -0400

    chore: Update app module imports and add TypeORM configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,41 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config();
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { WorkyNotificationsModule } from './worky-notifications/worky-notifications.module';
+import { WorkyNotificationsModule } from './socket-notifications/worky-notifications.module';
+import { NotificationsModule } from './modules/notifications/notifications.module';
 import * as cors from 'cors';
 
 @Module({
-  imports: [WorkyNotificationsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      migrations: ['dist/migrations/*{.ts,.js}'],
+      migrationsRun: true,
+      keepConnectionAlive: true,
+      autoLoadEntities: true,
+      synchronize: true,
+      ssl: process.env.DB_SSL === 'true',
+      extra: {
+        ssl:
+          process.env.DB_SSL === 'true'
+            ? {
+                rejectUnauthorized: false,
+              }
+            : null,
+      },
+    }),
+    WorkyNotificationsModule,
+    NotificationsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ async function bootstrap() {
   const logger = new Logger('Main');
   const app = await NestFactory.create(AppModule);
 
+  app.setGlobalPrefix('api/v1');
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
